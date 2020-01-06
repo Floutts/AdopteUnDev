5513fc47c9284bad56d831360ca4dff98640d5fc
Maxence <maxence.maziere@epsi.fr>
2020-01-06 15:41:23 +0000

    navbar

--- src/config/routing.php
+++ src/config/routing.php
@@ -2,9 +2,23 @@
 function getPage(){
 
     $lesPages['accueil'] = "actionAccueil";
+    $lesPages['apropos'] = "actionApropos";
+    $lesPages['mentions'] = "actionMentions";
+    $lesPages['contact'] = "actionContact";
+    $lesPages['profil'] = "actionProfil";
+    $lesPages['connexion'] = "actionConnexion";
 
-    $contenu = $lesPages['accueil'];
-    // La fonction envoie le contenu
+    if(isset($_GET['page'])){
+        $page = $_GET['page'];
+    }
+    else{
+        $page = 'accueil';
+    }
+    if (!isset($lesPages[$page])){
+        $page = 'accueil';
+    }
+    $contenu = $lesPages[$page];
     return $contenu;
+
 }
 ?>
--- src/controleur/controleur_index.php
+++ src/controleur/controleur_index.php
@@ -3,3 +3,23 @@
 function actionAccueil($twig) {
     echo $twig->render('index.html.twig', array());
 }
+
+function actionApropos($twig) {
+    echo $twig->render('apropos.html.twig', array());
+}
+
+function actionMentions($twig) {
+    echo $twig->render('mentions.html.twig', array());
+}
+
+function actionContact($twig) {
+    echo $twig->render('contact.html.twig', array());
+}
+
+function actionProfil($twig) {
+    echo $twig->render('profil.html.twig', array());
+}
+
+function actionConnexion($twig) {
+    echo $twig->render('connexion.html.twig', array());
+}
