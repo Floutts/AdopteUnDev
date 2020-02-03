9fa8bbac5087ca3886c2fa7bcb8025323f918378
Fabien <fabien.bayon@epsi.fr>
2020-02-03 07:38:48 +0000

    bd refait

--- src/bd/AdopteUnDev.sql
+++ src/bd/AdopteUnDev.sql
@@ -57,10 +57,11 @@ CREATE TABLE `developpeur` (
   `NbUnique` varchar(13) NOT NULL,
   `idRole` int(11) NOT NULL,
   `validation` tinyint(1) NOT NULL,
+  `dateInscrit` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   KEY `idRole` (`idRole`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,7 +70,7 @@ CREATE TABLE `developpeur` (
 
 LOCK TABLES `developpeur` WRITE;
 /*!40000 ALTER TABLE `developpeur` DISABLE KEYS */;
-INSERT INTO `developpeur` VALUES (2,'alphonse','gluiglui','a.g@fr','$2y$10$/63RwuPjVbSMGlVNFbBQCuwSt0Dg1oH7o6739cXzmVJgSp4nMYkSO','5e20393176315',2,0),(4,'Jean','Bon','Jambon@gmail.com','$2y$10$cgS1xLhjZlFecBaQfIxbNug4NQAx/G2fvGoBhFwPIfTAH5I5Xhwae','5e26adabc2500',2,0),(6,'Harry','Cauvert','Haricot.vert@gmail.com','$2y$10$Expnrsv0tfOMLotGgZdS3ONpBJIlJkQ81oYW8klm69Tw0TQ8fILXy','5e26af2ed620f',2,0),(19,'Bayon','Fabien','fabienbayon@yahoo.fr','$2y$10$wGl9G9lHy6ReZGTlPwQZhem0e1lxVjvVNR0xTdCGvbqfpNGAZZaBe','5e26cd9a0f0db',1,1);
+INSERT INTO `developpeur` VALUES (2,'alphonse','gluiglui','a.g@fr','$2y$10$/63RwuPjVbSMGlVNFbBQCuwSt0Dg1oH7o6739cXzmVJgSp4nMYkSO','5e20393176315',2,0,'0000-00-00 00:00:00'),(4,'Jean','Bon','Jambon@gmail.com','$2y$10$cgS1xLhjZlFecBaQfIxbNug4NQAx/G2fvGoBhFwPIfTAH5I5Xhwae','5e26adabc2500',2,0,'0000-00-00 00:00:00'),(6,'Harry','Cauvert','Haricot.vert@gmail.com','$2y$10$Expnrsv0tfOMLotGgZdS3ONpBJIlJkQ81oYW8klm69Tw0TQ8fILXy','5e26af2ed620f',2,0,'0000-00-00 00:00:00'),(30,'Bayon','fabien','fabienbayon@yahoo.fr','$2y$10$IGctYnaXTt4VPZmZYr5LR.hSNcqs/JI2MrYEnpsJkxNvkp51m6loG','5e2eb3b832810',1,1,'2020-01-27 09:56:08');
 /*!40000 ALTER TABLE `developpeur` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -129,4 +130,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-01-21 10:14:38
+-- Dump completed on 2020-02-03  7:34:01
