0775e2973e161fd843a2ed0a72355325a88a44e7
Maxence <maxence.maziere@epsi.fr>
2020-02-06 08:51:20 +0000

    renvoi email

--- src/controleur/controleur_index.php
+++ src/controleur/controleur_index.php
@@ -59,10 +59,20 @@ function actionConnexion($twig,$db) {
                 $form['message']='Login ou mot de passe incorrect';
             }
             else{
-                $_SESSION['login'] = $Email;
-                $_SESSION['role'] = $unDeveloppeur['idRole'];
+                $unDeveloppeur=$developpeur->selectByEmail($Email);
+                if ($unDeveloppeur['validation'] == 0){
 
-                header("Location:index.php");
+                    $form['valide']=false;
+                    $form['valideEmail']=false;
+                    $form['message']="Votre email n'a pas été vérifié";
+
+                }else{
+                    $_SESSION['login'] = $Email;
+                    $_SESSION['role'] = $unDeveloppeur['idRole'];
+
+                    header("Location:index.php");
+                }
+
             }
         }
         else{
@@ -70,6 +80,37 @@ function actionConnexion($twig,$db) {
             $form['message'] ='Login ou mot de passe incorrect';
         }
     }
+    if (isset($_POST['btRenvoi'])){
+        $form['renvoi'] = false;
+    }
+    if (isset($_POST['btRenvoiEmail'])) {
+        $Email = $_POST['Email'];
+        $developpeur = new Developpeur($db);
+        $unDeveloppeur=$developpeur->selectByEmail($Email);
+        $email = $unDeveloppeur['email'];
+        $nbUnique = $unDeveloppeur['nbUnique'];
+
+        $serveur = $_SERVER['HTTP_HOST'];
+        $script = $_SERVER["SCRIPT_NAME"];
+        $message = "
+            <html>
+                <head>
+                </head>
+                <body>
+                Bienvenue sur AdopteUnDev, Pour confirmer votre inscription, veuillez cliquer sur le lien ci contre           
+                <a href=http://$serveur$script?page=validation&email=$email&nbUnique=$nbUnique'>Valider votre inscription</a>
+                </body>
+            </html>"
+        ;
+        $headers[] = 'MIME-Version: 1.0';
+        $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+        //$message = wordwrap($message, 70, "\r\n");
+        mail($email, 'Inscription AdopteUnDev', $message, implode("\n",$headers));
+        $form['renvoi']=true;
+        $form['message']= "Un mail a été renvoyé";
+
+    }
+
         echo $twig->render('connexion.html.twig', array('form'=>$form));
 }
 
--- src/modele/class_developpeur.php
+++ src/modele/class_developpeur.php
@@ -13,7 +13,7 @@ class Developpeur {
         $this->db = $db;
         $this->insert= $db->prepare("insert into developpeur(email, mdp, nom, prenom, idRole,NbUnique,validation,dateInscrit) values(:email, :mdp, :nom, :prenom, :role, :NbUnique, :validation, :dateInscrit)"); ;
         $this->select= $db->prepare("select  `nom`, `prenom`, `email` FROM developpeur d WHERE email=:email");
-        $this->selectByEmail = $db->prepare("select nom,prenom,email,nbUnique,dateInscrit from developpeur d where email=:email"); // penserz a utilisé select by email pour la profil et mettre tout ce qu'on veux dedans .
+        $this->selectByEmail = $db->prepare("select nom,prenom,email,nbUnique,dateInscrit,validation from developpeur d where email=:email"); // penserz a utilisé select by email pour la profil et mettre tout ce qu'on veux dedans .
         $this->updateValidation= $db->prepare("update developpeur set validation=true where email=:email ");
         $this->connect= $db->prepare("select email, mdp from developpeur where email=:email");
         $this->updateMdp= $db->prepare("update developpeur set mdp=:mdp where email=:email");
